593a1778bc86b1fc772baa5e8db71559528701ab
Lê Đặng Phương Uyên <renyuiko.hamyana@gmail.com>
2022-09-28 19:14:22 +0700

    Add project files.

---
+++ MoneyKeeper/Controllers/SignUpController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MoneyKeeper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignUpController : ControllerBase
+    {
+        // GET: api/<SignUpController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SignUpController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SignUpController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SignUpController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SignUpController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ MoneyKeeper/SendMailService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Options;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoneyKeeper
+{
+    public class SendMailService
+    {
+        MailSettings _mailSettings { get; set; }
+        public SendMailService(IOptions<MailSettings> mailSettings)
+        {
+            _mailSettings = mailSettings.Value;
+        }
+        public async Task<string> SendMail(MailContent mailContent)
+        {
+            var email = new MimeMessage();
+            email.Sender = new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail);
+            email.From.Add(new MailboxAddress(_mailSettings.DisplayName, _mailSettings.Mail));
+            email.To.Add(new MailboxAddress(mailContent.To, mailContent.To));
+            email.Subject = mailContent.Subject;
+            var builder = new BodyBuilder();
+            builder.HtmlBody = mailContent.Body;
+            email.Body = builder.ToMessageBody();
+            using var smtp = new MailKit.Net.Smtp.SmtpClient();
+            try {
+                await smtp.ConnectAsync(_mailSettings.Host, _mailSettings.Port, MailKit.Security.SecureSocketOptions.StartTls);
+                await smtp.AuthenticateAsync(_mailSettings.Mail, _mailSettings.Password);
+                await smtp.SendAsync(email);
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                return "Error" + e.Message;
+            }
+          
+
+            smtp.Disconnect(true);
+            return "Success";
+
+        }
+    }
+
+    public class MailContent
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
