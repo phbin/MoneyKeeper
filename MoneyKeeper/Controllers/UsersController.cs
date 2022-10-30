f10a41457d19025ec8aa2332cb920348f35bd034
Lê Đặng Phương Uyên <renyuiko.hamyana@gmail.com>
2022-10-31 01:07:34 +0700

    get list + get user by id

--- MoneyKeeper/Controllers/UsersController.cs
+++ MoneyKeeper/Controllers/UsersController.cs
@@ -1,12 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoneyKeeper.Models;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace MoneyKeeper.Controllers
 {
+    [ApiController]
+    [Route("users")]
     public class UsersController : Controller
     {
-        public IActionResult Index()
+        public DataContext _context { get; set; }
+        public UsersController(DataContext context)
         {
-            return View();
+            _context = context;
+        }
+        [HttpGet("list-users")]
+        public async Task<IActionResult> GetUser()
+        {
+            return Ok(new
+            {
+                data = await _context.Users.OrderBy(u => u.id).ToListAsync()
+            });
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserByID(string id)
+        {
+            try
+            {
+                var user = await _context.Users.FirstOrDefaultAsync(u => u.id == Guid.Parse(id));
+                if(user == null) return NotFound();
+                return Ok(user);
+            }
+            catch
+            {
+                 return BadRequest();
+            }
         }
     }
 }
