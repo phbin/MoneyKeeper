24456486cec6f222fedbab29e30e4fdd6a40903b
Lê Đặng Phương Uyên <renyuiko.hamyana@gmail.com>
2023-02-03 20:00:32 +0700

    add event controller

---
+++ MoneyKeeper/Controllers/Events/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MoneyKeeper.Controllers.Events
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
